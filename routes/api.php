cf427036cd621e638f40cafea8a952a8ce549bbd
fhraju <alonghuman@gmail.com>
2022-12-26 21:46:40 +0600

    CRUDE Complete

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,28 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::resource('products', ProductController::class);
+
+// Search for the product
+Route::get('/products/search/{name}', [ProductController::class, 'search']);
+
+// // Show all the products
+// Route::get('/products', [ProductController::class, 'index']);
+
+// // Show single products
+// Route::get('/products/{product}', [ProductController::class, 'show']);
+
+// // Create product
+// Route::post('/products', [ProductController::class, 'store']);
+
+// // update product
+// Route::put('/products/{product}', [ProductController::class, 'update']);
+
+// // Delete product
+// Route::delete('/products/{product}', [ProductController::class, 'destroy']);
+
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
